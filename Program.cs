3246d5c5eff04bfd3f7db251637e58168533e3e3
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-03-13 08:37:30 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore_circleci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
